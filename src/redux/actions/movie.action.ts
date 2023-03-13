ad5282cf3099a8a88c7a98cc5a53d60f96f84486
Cruiz123 <64165501+Cruiz123@users.noreply.github.com>
2023-03-13 13:05:03 -0600

    Update dispacher for actions redux
    Updating action addMovies for event dispacher in method dispatch

--- src/redux/actions/movie.action.ts
+++ src/redux/actions/movie.action.ts
@@ -1,12 +1,12 @@
 import { ADDMOVIE, FILTERMOVIE } from './actionsTypes'
 
 export const addMovies = (data: any) => (dispatch: any) => {
-    dispatch({ type: ADDMOVIE, params: data })
+    dispatch({ type: ADDMOVIE, payload: data })
 }
 
 export const filterMovie = (data: any) => (dispatch: any) => {
     dispatch({
         type: FILTERMOVIE,
-        params: data,
+        payload: data,
     })
 }
